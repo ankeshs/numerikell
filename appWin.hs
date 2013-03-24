c94a87bba215145c7ed0b25e86fe36dc4f9e7bc6
Ankesh Kumar Singh <ank.singh90@gmail.com>
2013-03-24 19:17:30 +0530

    Working command terminal and addition of commands

--- appWin.hs
+++ appWin.hs
@@ -8,24 +8,40 @@ main = do
     initGUI
     builder <- builderNew
     builderAddFromFile builder "appWin.glade"
+    
+    --Binding components
     appWindow <- builderGetObject builder castToWindow "appWindow"
     exitDialog <- builderGetObject builder castToDialog "exitDialog"    
     terminal <- builderGetObject builder castToTextView "terminal"
-    textViewSetEditable terminal False
-    buf <- textViewGetBuffer terminal
-    textBufferSetText buf ">> "
+    cmdHist <- builderGetObject builder castToTextView "cmdHist"
+    varHist <- builderGetObject builder castToTextView "varHist"
     inp <- builderGetObject builder castToEntry "inp"    
+    
+    --Setting Properties and initializations
+    textViewSetEditable terminal False
+    textViewSetEditable cmdHist False
+    buf <- textViewGetBuffer terminal
+    cbuf <- textViewGetBuffer cmdHist
+    textBufferSetText buf ">> "
+    ei <- textBufferGetEndIter buf
+    em <- textBufferCreateMark buf Nothing ei False    
+    cei <- textBufferGetEndIter cbuf
+    
+    --Events
     afterEntryActivate inp $ do
 	 command <- entryGetText inp
 	 putStrLn command
 	 result <- return (getResult command)
 	 putStrLn result
-	 entrySetText inp ""
-	 ei <- textBufferGetEndIter buf
-	 textBufferInsert buf ei (command ++ "\n" ++ result ++ "\n>> ")
-    onDestroy appWindow mainQuit    
+	 entrySetText inp ""	 
+	 textBufferInsert buf ei (command ++ "\n" ++ result ++ "\n>> ")		 	 	 
+	 scstate <- textViewScrollToMark terminal em 0.0 Nothing
+	 textBufferInsert cbuf cei (command ++ "\n")
+    onDestroy appWindow mainQuit
+    
     widgetShowAll appWindow
     mainGUI
     
+--Takes the command entered and returns output, may additionally take environment with variables
 getResult :: String -> String
 getResult _ = "Result unknown"
